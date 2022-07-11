dcfac7846dcdb71f95f6f3407246185a7b94729d
Martin Zheng <jszhengyq@gmail.com>
2022-07-11 14:53:20 -0700

    Updated docker config

--- docker/run.sh
+++ docker/run.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
-VOLUMN_PATH="/Users/martinz/linux-dev/mole-core"
-docker run --rm -ti --env RUNNER=${RUNNER:-start} -v $VOLUMN_PATH:/dermis -p ${PORT:-3000}:3000 jszhengyq/dermis
+
+# 
+# Params definition:
+# $CMD: a script comand defined in package.json's "scripts" section
+# $DERMIS_VOLUMN_PATH: the local dev path of the code directory where the docker container runs 
+# $PORT: React dev server's URL port number 
+#
+# Usage example:
+#```
+# sh docker/run.sh
+#
+# SCRIPT=test sh docker/run.sh
+# SCRIPT=start
+# ```
+#
+
+docker run --rm -ti --env SCRIPT=${SCRIPT:-start} -v $MOLE_VOLUMN_PATH:/dermis -p ${PORT:-3000}:3000 jszhengyq/dermis
